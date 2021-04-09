e69498410ab49c89867efc3a37d269a4d9c305fa
Ben Shekhtman <benshekhtman@hotmail.com>
2021-04-08 21:20:33 -0400

    functionnality working. adding final css

--- JS/main.js
+++ JS/main.js
@@ -1,5 +1,8 @@
 let ul = document.querySelector('.list-items')
+const list = ul.children;
+const div = document.querySelector('.list')
 
+function makeBtn() {
 const submitBtn = document.getElementById('button')
     submitBtn.addEventListener('click', function(e){
         const item = document.createElement('li');
@@ -7,8 +10,35 @@ const submitBtn = document.getElementById('button')
         item.textContent = input.value; 
         if(input.value === '') {
         return
-         }else{
+        }else{
             ul.appendChild(item);
             input.value = '';
+            createDltBtn(item)   
         }
     })
+}
+makeBtn()
+
+
+function createDltBtn (li){
+    const remove = document.createElement('button')
+    remove.className = 'btn-remove'
+    remove.innerHTML = 'X'
+    li.appendChild(remove)
+    return li;
+}
+for(var i = 0; i < list.length; i ++){
+    createDltBtn(list[i])
+}
+
+div.addEventListener('click', (e) => {
+    if(e.target.tagName === 'BUTTON') {
+        const button = e.target;
+        const li = button.parentNode;
+        const ul = li.parentNode;
+        if(button.className === 'btn-remove') {
+            alert('are you sure?')
+            ul.removeChild(li);
+        }
+    }
+})
